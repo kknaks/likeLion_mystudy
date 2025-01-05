03ce425815eddda802bf12d99a82f65248d20fbe
kknaks <benesia93@naver.com>
2025-01-05 21:22:56 +0900

    jumptospringboot chapter3

--- APP/JumpToSpringBoot/jump/src/main/java/com/ll/jump/Answer/controller/AnswerController.java
+++ APP/JumpToSpringBoot/jump/src/main/java/com/ll/jump/Answer/controller/AnswerController.java
@@ -49,7 +49,10 @@ public class AnswerController {
 
     this.answerService.create(question, answerForm.getContent(), siteUser);
 
-    return String.format("redirect:/question/detail/%d", id);
+    Answer answer = this.answerService.create(question,
+        answerForm.getContent(), siteUser);
+    return String.format("redirect:/question/detail/%s#answer_%s",
+        answer.getQuestion().getId(), answer.getId());
   }
 
   @PreAuthorize("isAuthenticated()")
@@ -86,7 +89,8 @@ public class AnswerController {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
     }
     this.answerService.delete(answer);
-    return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    return String.format("redirect:/question/detail/%s#answer_%s",
+        answer.getQuestion().getId(), answer.getId());
   }
 
   @PreAuthorize("isAuthenticated()")
@@ -95,6 +99,7 @@ public class AnswerController {
     Answer answer = this.answerService.getAnswer(id);
     SiteUser siteUser = this.userService.getUser(principal.getName());
     this.answerService.vote(answer, siteUser);
-    return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    return String.format("redirect:/question/detail/%s#answer_%s",
+        answer.getQuestion().getId(), answer.getId());
   }
 }

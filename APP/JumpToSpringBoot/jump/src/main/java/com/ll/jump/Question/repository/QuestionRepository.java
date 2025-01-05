e4184933f7cd88da9d7eebda3080f47ce9b70f33
kknaks <benesia93@naver.com>
2025-01-05 21:39:58 +0900

    jumptospringboot chapter3

--- APP/JumpToSpringBoot/jump/src/main/java/com/ll/jump/Question/repository/QuestionRepository.java
+++ APP/JumpToSpringBoot/jump/src/main/java/com/ll/jump/Question/repository/QuestionRepository.java
@@ -1,8 +1,12 @@
 package com.ll.jump.Question.repository;
 
+import com.ll.jump.Answer.entity.Answer;
 import com.ll.jump.Question.entity.Question;
+import com.ll.jump.user.entity.SiteUser;
+import jakarta.persistence.criteria.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +16,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
   Question findBySubjectAndContent(String subject, String content);
   List<Question> findBySubjectLike(String subject);
   Page<Question> findAll(Pageable pageable);
+  Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
